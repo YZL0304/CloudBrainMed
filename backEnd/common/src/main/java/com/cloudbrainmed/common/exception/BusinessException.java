06ed7cd33132c9a312deca62d3e7c61c6f903d96
YZL0304 <2918386159@qq.com>
2026-06-05 23:05:38 +0800

    Revert "修复"

--- backEnd/common/src/main/java/com/cloudbrainmed/common/exception/BusinessException.java
+++ backEnd/common/src/main/java/com/cloudbrainmed/common/exception/BusinessException.java
@@ -1,17 +1,4 @@
 package com.cloudbrainmed.common.exception;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
-=======
 public class BusinessException {
 }
->>>>>>> 2247f8ba28557bb5f914504fbd9c1661716886cf
-=======
-public class BusinessException {
-}
->>>>>>> 2247f8ba28557bb5f914504fbd9c1661716886cf
--- backEnd/services/doctor-service/src/main/java/com/cloudbrainmed/doctor/controller/DoctorProfileController.java
+++ backEnd/services/doctor-service/src/main/java/com/cloudbrainmed/doctor/controller/DoctorProfileController.java
@@ -1,20 +1,6 @@
 package com.cloudbrainmed.doctor.controller;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import com.cloudbrainmed.common.result.Result;
-import com.cloudbrainmed.doctor.service.DoctorProfileService;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Map;
-=======
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 2247f8ba28557bb5f914504fbd9c1661716886cf
-=======
-import org.springframework.web.bind.annotation.RestController;
->>>>>>> 2247f8ba28557bb5f914504fbd9c1661716886cf
 
 @RestController
 public class DoctorProfileController {
