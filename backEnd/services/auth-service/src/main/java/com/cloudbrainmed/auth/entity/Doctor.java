afc6c374da4cf9e0f260cd40694a96b88aaf8d60
liuIRIS <15945328+liuiris@user.noreply.gitee.com>
2026-06-09 01:38:01 +0800

    我的代码

--- backEnd/services/auth-service/src/main/java/com/cloudbrainmed/auth/entity/Doctor.java
+++ backEnd/services/auth-service/src/main/java/com/cloudbrainmed/auth/entity/Doctor.java
@@ -1,4 +1,37 @@
 package com.cloudbrainmed.auth.entity;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Doctor {
+    // 医生ID
+    private String doctorId;
+    // 头像
+    private String avatar;
+    // 姓名
+    private String name;
+    // 性别
+    private Integer gender;
+    // 电话
+    private String phone;
+    // 邮箱
+    private String email;
+    // 职位
+    private String position;
+    // 擅长
+    private String goodAt;
+    // 简介
+    private String introduction;
+    // 密码
+    private String password;
+    // 科室ID
+    private String departmentId;
+    // 0停用 1正常
+    private Integer status;
+    // 创建时间
+    private Date createTime;
+    // 0未删除 1已删除
+    private Integer isDeleted;
 }
